243abce50169dc301ceb12c2f119be5432ec5808
Dianliang <dianliang233@gmail.com>
2020-04-19 14:11:01 +0800

    API avatar

--- docs/src/js/template.js
+++ docs/src/js/template.js
@@ -3,3 +3,14 @@ if (window.self !== window.top) {
   $('body').addClass('iframe');
   $('head').append('<base target="_blank">');
 }
+$.get("https://api.github.com/repos/dragon-fish/inpageedit-v2/contributors").then(function(contribdata){
+  var contribdata = contribdata || [];
+  for (var i = 0; i < contribdata.length & i < 6; i++){
+    $(".avatar-stack").append(`<img src=\"${contribdata[i]["avatar_url"]}\" width="64px" class=\"mdui-img-circle\" mdui-tooltip=\"\{\'content\':\'${contribdata[i]["login"]}\'\}\">`)
+  };
+  if (contribdata.length > 6){
+    var mousrno = contribdata.length - 6
+    $(".avatar-stack").prepend(`<a href=\"https://github.com/Dragon-Fish/InPageEdit-v2/graphs/contributors\"><div class=\"avatar-stack-overflow\">+${mousrno}</div></a>`)
+  }
+  mdui.mutation()
+})
